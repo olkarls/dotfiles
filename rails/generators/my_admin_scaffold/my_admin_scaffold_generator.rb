ec2531ee3d014be5a8abf41192c1f5a9a6a20e98
Ola Karlsson <ola@usefulinnovation.se>
2009-08-03 15:13:15 +0200

    Initial commit

---
+++ rails/generators/my_admin_scaffold/my_admin_scaffold_generator.rb
@@ -0,0 +1,157 @@
+class MyAdminScaffoldGenerator < Rails::Generator::NamedBase
+  default_options :skip_timestamps => false, :skip_migration => false, :force_plural => false
+
+  attr_reader   :controller_name,
+                :controller_class_path,
+                :controller_file_path,
+                :controller_class_nesting,
+                :controller_class_nesting_depth,
+                :controller_class_name,
+                :controller_underscore_name,
+                :controller_singular_name,
+                :controller_plural_name
+  alias_method  :controller_file_name,  :controller_underscore_name
+  alias_method  :controller_table_name, :controller_plural_name
+
+  def initialize(runtime_args, runtime_options = {})
+    super
+
+    if @name == @name.pluralize && !options[:force_plural]
+      logger.warning "Plural version of the model detected, using singularized version.  Override with --force-plural."
+      @name = @name.singularize
+    end
+
+    @controller_name = @name.pluralize
+
+    base_name, @controller_class_path, @controller_file_path, @controller_class_nesting, @controller_class_nesting_depth = extract_modules(@controller_name)
+    @controller_class_name_without_nesting, @controller_underscore_name, @controller_plural_name = inflect_names(base_name)
+    @controller_singular_name=base_name.singularize
+    if @controller_class_nesting.empty?
+      @controller_class_name = @controller_class_name_without_nesting
+    else
+      @controller_class_name = "#{@controller_class_nesting}::#{@controller_class_name_without_nesting}"
+    end
+  end
+
+  def manifest
+    record do |m|
+      # Check for class naming collisions.
+      m.class_collisions("#{controller_class_name}Controller", "#{controller_class_name}Helper")
+      m.class_collisions(class_name)
+
+      # Controller, helper, views, test and stylesheets directories.
+      m.directory(File.join('app/models', class_path))
+      m.directory(File.join('app/controllers', controller_class_path))
+      m.directory(File.join('app/helpers', controller_class_path))
+      m.directory(File.join('app/views', controller_class_path, controller_file_name))
+      m.directory(File.join('app/views/layouts', controller_class_path))
+      m.directory(File.join('test/functional', controller_class_path))
+      m.directory(File.join('test/unit', class_path))
+      m.directory(File.join('test/unit/helpers', class_path))
+      m.directory(File.join('public/stylesheets', class_path))
+      
+      m.directory(File.join('app/controllers/admin', controller_class_path))
+      m.directory(File.join('app/helpers/admin', controller_class_path))
+      m.directory(File.join('app/views/admin', controller_class_path, controller_file_name))
+
+      for action in scaffold_views
+        m.template(
+          "public_view_#{action}.html.erb",
+          File.join('app/views', controller_class_path, controller_file_name, "#{action}.html.erb")
+        )
+      end
+      
+      for action in admin_views
+        m.template(
+          "view_#{action}.html.erb",
+          File.join('app/views/admin', controller_class_path, controller_file_name, "#{action}.html.erb")
+        )
+      end
+      
+      m.template "view_form.html.erb", File.join('app/views/admin', controller_class_path, controller_file_name, "_form.html.erb")
+      m.template(
+        'controller.rb', File.join('app/controllers/admin', controller_class_path, "#{controller_file_name}_controller.rb")
+      )
+      
+      m.template(
+        'public_controller.rb', File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
+      )
+      
+      m.template('functional_test.rb', File.join('test/functional', controller_class_path, "#{controller_file_name}_controller_test.rb"))
+      m.template('helper.rb',          File.join('app/helpers',     controller_class_path, "#{controller_file_name}_helper.rb"))
+      m.template('helper_test.rb',     File.join('test/unit/helpers',    controller_class_path, "#{controller_file_name}_helper_test.rb"))
+
+      m.route_my_resources controller_file_name
+      m.route_namespaced_resources :admin, controller_file_name
+
+      m.dependency 'model', [name] + @args, :collision => :skip
+    end
+  end
+
+  protected
+    # Override with your own usage banner.
+    def banner
+    "Usage: #{$0} my_scaffold ModelName [field:type, field:type]"
+  end
+
+  def add_options!(opt)
+    opt.separator ''
+    opt.separator 'Options:'
+    opt.on("--skip-timestamps",
+           "Don't add timestamps to the migration file for this model") { |v| options[:skip_timestamps] = v }
+    opt.on("--skip-migration",
+           "Don't generate a migration file for this model") { |v| options[:skip_migration] = v }
+    opt.on("--force-plural",
+           "Forces the generation of a plural ModelName") { |v| options[:force_plural] = v }
+  end
+
+  def scaffold_views
+    %w[ index show ]
+  end
+  
+  def admin_views
+    %w[ new edit index show confirm_destruction ]
+  end
+
+  def model_name
+    class_name.demodulize
+  end
+  
+  def route_my_resources(*resources)
+    resource_list = resources.map { |r| r.to_sym.inspect }.join(', ')
+    sentinel = 'ActionController::Routing::Routes.draw do |map|'
+    logger.route "map.resources #{resource_list}"
+    unless options[:pretend]
+      gsub_file 'config/routes.rb', /(#{Regexp.escape(sentinel)})/mi do |match|
+        "#{match}\n  map.resources #{resource_list}, :only => [:index, :show]\n"
+      end
+    end
+=begin
+    resource_list = resources.map { |r| r.to_sym.inspect }.join(', ')
+    sentinel = 'ActionController::Routing::Routes.draw do |map|'
+    logger.route "map.resources #{resource_list}, :member => { :confirm_destruction => :get }"
+    unless options[:pretend]
+      gsub_file 'config/routes.rb', /(#{Regexp.escape(sentinel)})/mi do |match|
+        "#{match}\n  map.resources #{resource_list}, :member => { :confirm_destruction => :get }\n"
+      end
+    end
+=end
+  end
+  
+  def route_namespaced_resources(namespace, *resources)
+    resource_list = resources.map { |r| r.to_sym.inspect }.join(', ')
+    sentinel = 'ActionController::Routing::Routes.draw do |map|'
+    logger.route "#{namespace}.resources #{resource_list}"
+    unless options[:pretend]
+      gsub_file 'config/routes.rb', /(#{Regexp.escape(sentinel)})/mi do |match|
+        "#{match}\n  map.namespace(:#{namespace}) do |#{namespace}|\n     #{namespace}.resources #{resource_list}, :member => { :confirm_destruction => :get }\n  end\n"
+      end
+    end
+  end
+  
+  def gsub_file(relative_destination, regexp, *args, &block)
+    path = destination_path(relative_destination)
+    content = File.read(path).gsub(regexp, *args, &block)
+    File.open(path, 'wb') { |file| file.write(content) }
+  end
+end
